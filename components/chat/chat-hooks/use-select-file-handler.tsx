48f80a036862b945a89d1d7cbae2580af4dbe861
canyadigit29 <canyadigit29@gmail.com>
2025-06-15 23:07:17 -0400

    who knows

--- components/chat/chat-hooks/use-select-file-handler.tsx
+++ components/chat/chat-hooks/use-select-file-handler.tsx
@@ -164,8 +164,7 @@ export const useSelectFileHandler = () => {
                 tokens: 0,
                 type: simplifiedFileType
               },
-              selectedWorkspace.id,
-              chatSettings.embeddingsProvider
+              selectedWorkspace.id
             )
 
             setFiles(prev => [...prev, createdFile])
